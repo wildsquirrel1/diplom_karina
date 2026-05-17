bfbd864cba9aa90c79b759dc19e4c3b77d901cc8
wildsquirrel1 <ismuhametovakarina5@gmail.com>
2026-05-17 13:45:37 +0500

    дизайн (не нравится)

--- hotel/hotel/BookingControl.xaml.cs
+++ hotel/hotel/BookingControl.xaml.cs
@@ -66,6 +66,11 @@ namespace hotel
                     btnCancel.Visibility = Visibility.Collapsed;
                     break;
 
+                case 4:
+                    statusBadge.Background = new SolidColorBrush(Color.FromRgb(15, 82, 186));
+                    btnCancel.Visibility = Visibility.Collapsed;
+                    break;
+
                 default:
                     statusBadge.Background = new SolidColorBrush(Colors.Gray);
                     btnCancel.Visibility = Visibility.Collapsed;
